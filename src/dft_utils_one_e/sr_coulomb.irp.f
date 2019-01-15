5c8d1523b75e6e93a4b74f6824026cf221d60ece
Emmanuel Giner <giner.emmanuel@gmail.com>
2019-01-15 20:19:08 +0100

    minor modifs

--- src/dft_utils_one_e/sr_coulomb.irp.f
+++ src/dft_utils_one_e/sr_coulomb.irp.f
@@ -35,34 +35,3 @@
  short_range_Hartree = short_range_Hartree * 0.5d0
  print*, 'short_range_Hartree',short_range_Hartree
 END_PROVIDER
-
-
- BEGIN_PROVIDER [double precision, effective_one_e_potential, (mo_num, mo_num,N_states)]
-&BEGIN_PROVIDER [double precision, effective_one_e_potential_without_kin, (mo_num, mo_num,N_states)]
- implicit none
- integer :: i,j,istate
- effective_one_e_potential = 0.d0
- BEGIN_DOC
-! Effective_one_e_potential(i,j) = $\rangle i| v_{H}^{sr} |j\rangle  + \rangle i| h_{core} |j\rangle  + \rangle i|v_{xc} |j\rangle$
-!
-! Taking the expectation value does not provide any energy, but
-! effective_one_e_potential(i,j) is the potential coupling DFT and WFT part to
-! be used in any WFT calculation.
-!
-! shifted_effective_one_e_potential_without_kin = effective_one_e_potential_without_kin + shifting_constant on the diagonal
- END_DOC
- do istate = 1, N_states
-  do i = 1, mo_num
-   do j = 1, mo_num
-    effective_one_e_potential(i,j,istate) = short_range_Hartree_operator(i,j,istate) + mo_integrals_n_e(i,j) + mo_kinetic_integrals(i,j)    &
-                                   + 0.5d0 * (potential_x_alpha_mo(i,j,istate) + potential_c_alpha_mo(i,j,istate)                               &
-                                   +          potential_x_beta_mo(i,j,istate)  + potential_c_beta_mo(i,j,istate)   )
-    effective_one_e_potential_without_kin(i,j,istate) = short_range_Hartree_operator(i,j,istate) + mo_integrals_n_e(i,j)                   &
-                                   + 0.5d0 * (potential_x_alpha_mo(i,j,istate) + potential_c_alpha_mo(i,j,istate)                               &
-                                   +          potential_x_beta_mo(i,j,istate)  + potential_c_beta_mo(i,j,istate)   )
-   enddo
-  enddo
- enddo
-END_PROVIDER
-
-
